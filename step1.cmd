4cce6065ed8d42001a5388ee5d695bf4acb7b03e
MrWise <ydudarev@iconsoft.ru>
2020-12-30 13:31:02 +0600

    framework_4_32

--- step1.cmd
+++ step1.cmd
@@ -2,9 +2,7 @@ rem Check ARM Step 1
 rem git push -u armcheck master
 @echo off
 for /f "tokens=2 delims=:" %%a in ('powershell -Command Get-Host ^| findstr /c:Version') do (echo %%a)
-if exist "C:\Windows\Microsoft.NET\Framework\v4*" echo Framework_V4_exist  (
-echo folder EXIST
-) ELSE (
-echo No
-)
+
+if exist "C:\Windows\Microsoft.NET\Framework\v4*" echo Framework_V4_32_exist
+
 timeout 15
