a5463fe497e8d7f06d3ff72c60b47157ed0d5a8c
MrWise <ydudarev@iconsoft.ru>
2020-12-30 21:03:58 +0600

    step0

--- step0.bat
+++ step0.bat
@@ -1,4 +1,7 @@
-if exist "C:\Windows\Microsoft.NET\Framework\v4*" echo Framework_V4_32_exist
-if exist "C:\Windows\Microsoft.NET\Framework64\v4*" echo Framework_V4_64_exist
-netsh http add urlacl url=http://+:9000/ user=Everyone
-netsh http add urlacl url=http://+:9000/ user=все
+@echo off
+    for /f "tokens=2 delims=:" %%a in ('powershell -Command Get-Host ^| findstr /c:Version') do (echo pshell Version %%a)
+    if exist "C:\Windows\Microsoft.NET\Framework\v4*" echo Framework_V4_32_exist
+    if exist "C:\Windows\Microsoft.NET\Framework64\v4*" echo Framework_V4_64_exist
+    netsh http add urlacl url=http://+:9000/ user=Everyone
+    netsh http add urlacl url=http://+:9000/ user=все
+timeout 10
