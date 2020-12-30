8cd1a9d86a1cfad0b71a2f896759b7be44f4a4b7
MrWise <ydudarev@iconsoft.ru>
2020-12-30 21:06:11 +0600

    step1

--- step1.cmd
+++ step1.cmd
@@ -1,8 +1,5 @@
 rem Check ARM Step 1
 rem git push -u armcheck master
 @echo off
-    if exist "C:\Windows\Microsoft.NET\Framework\v4*" echo Framework_V4_32_exist
-    if exist "C:\Windows\Microsoft.NET\Framework64\v4*" echo Framework_V4_64_exist
-    netsh http add urlacl url=http://+:9000/ user=Everyone
-    netsh http add urlacl url=http://+:9000/ user=все
+
 timeout 15
