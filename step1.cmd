54e9fc5103103040214834c5898d7bef329f41a0
MrWise <ydudarev@iconsoft.ru>
2021-01-05 18:26:03 +0600

    splitet 2\

--- step1.cmd
+++ step1.cmd
@@ -1,9 +1,6 @@
 rem Check ARM Step 1
 rem git push -u armcheck master
 @echo off
-powershell
     $PSVersionTable.PSVersion
-TNC msk-fcc-vpn.tech.nis-glonass.ru -Port 80 -InformationLevel Quiet
-TNC msk-fcc-vpn.tech.nis-glonass.ru -Port 443 -InformationLevel Quiet
-TNC msk-fcc-vpn.tech.nis-glonass.ru -Port 5060 -InformationLevel Quiet
+    start powershell -noexit -file "С:\tmp\step2.ps1"
     timeout 15
---
+++ step2.ps1
@@ -0,0 +1,3 @@
+TNC msk-fcc-vpn.tech.nis-glonass.ru -Port 80 -InformationLevel Quiet
+TNC msk-fcc-vpn.tech.nis-glonass.ru -Port 443 -InformationLevel Quiet
+TNC msk-fcc-vpn.tech.nis-glonass.ru -Port 5060 -InformationLevel Quiet
