1f4ccdd57492024dfddacfc64d44a5096c59d4d8
ydudarev <ydudarev@iconsoft.ru>
2021-01-10 11:40:18 +0300

    step2_work

---
+++ restricted_HKLM_policy.bat
@@ -0,0 +1,1 @@
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "ExecutionPolicy" /t REG_SZ /d "Restricted" /f
--- step1.cmd
+++ step1.cmd
@@ -1,4 +1,2 @@
-﻿@echo off
-    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "ExecutionPolicy" /t REG_SZ /d "Restricted" /f
-        start powershell -noexit -file "С:\tmp\step2.ps1"
-    timeout 10
+﻿start powershell -noexit -executionpolicy bypass -command "C:step2.ps1"
+timeout 10
