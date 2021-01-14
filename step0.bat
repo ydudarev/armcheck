6a4c35b11e7f1163347f6965a41f8da1c1c9697b
ydudarev <ydudarev@iconsoft.ru>
2021-01-14 15:40:14 +0300

    final_v_0_1

--- step0.bat
+++ step0.bat
@@ -1,10 +1,11 @@
 @echo off
+    chcp 1251
     for /f "tokens=2 delims=:" %%a in ('powershell -Command Get-Host ^| findstr /c:Version') do (echo pshell Version %%a)
     if exist "C:\Windows\Microsoft.NET\Framework\v4*" echo Framework_V4_32_exist
     netsh http add urlacl url=http://+:9000/ user=Everyone
     netsh http add urlacl url=http://+:9000/ user=все
     echo %COMPUTERNAME% > C:\tmp\%COMPUTERNAME%_result.txt
-    echo ipconfig >> C:\tmp\%COMPUTERNAME%_result.txt
+    ipconfig >> C:\tmp\%COMPUTERNAME%_result.txt
     tcping -n 10 msk-fcc-vpn.tech.nis-glonass.ru 80 >> C:\tmp\%COMPUTERNAME%_result.txt
     tcping -n 10 msk-fcc-vpn.tech.nis-glonass.ru 443 >> C:\tmp\%COMPUTERNAME%_result.txt
     tcping -n 10 msk-fcc-vpn.tech.nis-glonass.ru 5060 >> C:\tmp\%COMPUTERNAME%_result.txt
