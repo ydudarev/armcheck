182acd4d291927b4765a6476ad3f52f73ced419f
MrWise <ydudarev@iconsoft.ru>
2021-01-21 18:54:50 +0600

    Update README.md

--- step0.bat
+++ step0.bat
@@ -1,5 +1,5 @@
 @echo off
-    chcp 1251
+    chcp 866
     for /f "tokens=2 delims=:" %%a in ('powershell -Command Get-Host ^| findstr /c:Version') do (echo pshell Version %%a)
     if exist "C:\Windows\Microsoft.NET\Framework\v4*" echo Framework_V4_32_exist
     netsh http add urlacl url=http://+:9000/ user=Everyone
@@ -7,7 +7,6 @@
     echo %COMPUTERNAME% > C:\tmp\%COMPUTERNAME%_result.txt
     ipconfig >> C:\tmp\%COMPUTERNAME%_result.txt
     tcping -n 10 msk-fcc-vpn.tech.nis-glonass.ru 80 >> C:\tmp\%COMPUTERNAME%_result.txt
-    tcping -n 10 msk-fcc-vpn.tech.nis-glonass.ru 443 >> C:\tmp\%COMPUTERNAME%_result.txt
     tcping -n 10 msk-fcc-vpn.tech.nis-glonass.ru 5060 >> C:\tmp\%COMPUTERNAME%_result.txt
 start powershell -noexit -executionpolicy bypass -command "C:step1.ps1"
 timeout 10
