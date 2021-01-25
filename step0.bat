f5458688ca5d7f3f2875ef82c83523fb6a460fe9
ydudarev <ydudarev@iconsoft.ru>
2021-01-25 11:49:54 +0300

    del443_add_key_d

--- step0.bat
+++ step0.bat
@@ -6,7 +6,7 @@
     netsh http add urlacl url=http://+:9000/ user=все
     echo %COMPUTERNAME% > C:\tmp\%COMPUTERNAME%_result.txt
     ipconfig >> C:\tmp\%COMPUTERNAME%_result.txt
-    tcping -n 10 msk-fcc-vpn.tech.nis-glonass.ru 80 >> C:\tmp\%COMPUTERNAME%_result.txt
-    tcping -n 10 msk-fcc-vpn.tech.nis-glonass.ru 5060 >> C:\tmp\%COMPUTERNAME%_result.txt
+    tcping -d -n 10 msk-fcc-vpn.tech.nis-glonass.ru 80 >> C:\tmp\%COMPUTERNAME%_result.txt
+    tcping -d -n 10 msk-fcc-vpn.tech.nis-glonass.ru 5060 >> C:\tmp\%COMPUTERNAME%_result.txt
 start powershell -noexit -executionpolicy bypass -command "C:step1.ps1"
-timeout 10
+timeout 5
